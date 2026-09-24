d400bb7397c1c1b396ff4f20c86a4e2aab4f3038
Shubham kumar <shubhofficial1@gmail.com>
2026-09-24 21:11:57 +0530

    Added Onboarding Screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <View className="flex-1 items-center justify-center gap-6 bg-white">
       <Text className="h1">Hello World</Text>
+      <Link href="/onboarding" className="h4 text-primary-deep!">
+        Open onboarding screen
+      </Link>
     </View>
   );
 }
